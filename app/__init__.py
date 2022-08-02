f037123fd52e0e1449d80a8799ad8c893b893656
cspe111 <cspe111@wgu.edu>
2022-08-02 12:49:22 -0400

    Initial commit

---
+++ app/__init__.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+from app import models
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+@app.route('/manager')
+def manager():
+    return render_template('manager.html')
+@app.route('/check-in')
+def checkin():
+    return render_template('check-in.html')
+@app.route('/learn')
+def learn():
+    return render_template('learn.html')
+
+@app.route('/employee')
+def index():
+    return render_template('employee.html')
+
+
+# Function that makes prediction
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 8)
+    loaded_model = pickle.load(open("model.pkl", "rb"))
+    results = loaded_model.predict(to_predict)
+    return results[0]
+
+# Function that returns prediction based on user input
+@app.route('/result', methods=['POST'])
+def result():
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        results = ValuePredictor(to_predict_list)
+
+        if int(results) == 0:
+            prediction = 'A conversation with your manager could benefit you!'
+        else:
+            prediction = 'A conversation with your manager is not necessary right now.'
+
+        return render_template("employee.html", prediction=prediction)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ app/models.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+df = pd.read_csv('mental-health-survey-cleaned.csv')
+
+
+df.drop(df[df['Age'] < 0].index, inplace=True)
+df.drop(df[df['Age'] > 100].index, inplace=True)
+
+df['Gender'].replace(['Male ', 'male', 'M', 'm', 'Male', 'Cis Male',
+                      'Man', 'cis male', 'Mail', 'Male-ish', 'Male (CIS)',
+                      'Cis Man', 'msle', 'Malr', 'Mal', 'maile', 'Make',], 'Male', inplace=True)
+
+df['Gender'].replace(['Female ', 'female', 'F', 'f', 'Woman', 'Female',
+                      'femail', 'Cis Female', 'cis-female/femme', 'Femake', 'Female (cis)',
+                      'woman', ], 'Female', inplace=True)
+
+df["Gender"].replace(['Female (trans)', 'queer/she/they', 'non-binary',
+                      'fluid', 'queer', 'Androgyne', 'Trans-female', 'male leaning androgynous',
+                      'Agender', 'A little about you', 'Nah', 'All',
+                      'ostensibly male, unsure what that really means',
+                      'Genderqueer', 'Enby', 'p', 'Neuter', 'something kinda male?',
+                      'Guy (-ish) ^_^', 'Trans woman', ], 'Other', inplace=True)
+
+df['work_interfere'] = df['work_interfere'].fillna('Not Sure')
+
+# Label Encoder
+categories =['Age', 'Gender', 'family_history', 'treatment', 'work_interfere', 'seek_help',
+               'mental_health_consequence', 'coworkers', 'supervisor']
+labelEncoder = preprocessing.LabelEncoder()
+
+# Maps all the numerical values to each categorical label
+mapping_dict = {}
+for col in categories:
+    df[col] = labelEncoder.fit_transform(df[col])
+    le_mapping = dict(zip(labelEncoder.classes_, labelEncoder.transform(labelEncoder.classes_)))
+    mapping_dict[col] = le_mapping
+
+X = df.values[:, 0:8]
+Y = df.values[:, 8]
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=100)
+decision_tree = DecisionTreeClassifier(criterion="gini", random_state=100, max_depth=5, min_samples_leaf=5)
+decision_tree.fit(X_train, y_train)
+y_prediction = decision_tree.predict(X_test)
+
+import pickle
+pickle.dump(decision_tree, open("model.pkl", "wb"))
